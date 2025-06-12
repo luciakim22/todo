6dc3bd376effea8b73c90197d120ed0e76cf44fb
luciakim22 <luciakim22@naver.com>
2025-06-13 08:08:23 +0900

    data

---
+++ data/seed.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import mockData from './mock.js'
+import Task from '../models/Task.js'
+import { DATABASE_URL } from '../env.js'
+
+await mongoose
+  .connect(DATABASE_URL)
+  .then(() => console.log('Connected to DB'))
+
+await Task.deleteMany({})
+await Task.insertMany(mockData.map((data) => {
+  delete data.id
+  return data
+}))
+
+// const mockInsertData = mockData.map((data) => {
+//   delete data.id
+//   return data
+// })
+
+// await Task.insertMany(mockInsertData)
+
+await mongoose.connection.close()
